2df72a27a582287c006635949b2316c8b63a2c4f
Igor Turanin <igor.t_95@yahoo.de>
2026-06-21 17:00:34 +0200

    update gradle and targetsdk(37)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.igtuapps.chaoscomputerstreams"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.igtuapps.chaoscomputerstreams"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
